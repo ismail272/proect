2d503b77d375d1338bda3bf4bf3ed1568e019cab
ismail <ismtim095@gmail.com>
2021-02-07 00:36:06 +0300

    ggwp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props) {
+       const [notes, setNotes] = useState( [
+        {
+            name: "Встать с дивана",
+            done: false
+        },
+        {
+            name: "Покушать",
+            done: true
+        },
+        {
+            name: "Лечь снова на диван",
+            done: true
+        }
+    ])
+
+    const [text, setText] = useState("")
+
+    const [check, setCheck] = useState(false)
+
+    const handleCheck = () => {
+        setCheck(!check)
+    }
+
+    const deleteNotes = (i) => {
+        const filter = notes.filter((note,index) => {
+            if (index === i) {
+                return false
+            }
+            return true
+        })
+        setNotes(filter)
+    }
+
+    const addNotes = () => {
+        setNotes([...notes,{
+            name: text,
+            done: check
+        }
+        ])
+        setText("")
+        setCheck(false)}
+
+    return (
+        <div className="app">
+            <Header />
+            <div className="input">
+                <input type="text"
+                       value={text}
+                       onChange={(e) => setText(e.target.value)}
+                       placeholder="Введите текст..."/>
+                <input type="checkbox" checked={check} onChange={handleCheck}/>
+                <button onClick={addNotes}><a href="#">ДОБАВИТЬ</a></button>
+            </div>
+            <div>{notes.map((note, index) => {
+                return(
+                    <div className="note" key={index}>
+                        <input type="checkbox" checked={note.done} onChange={() => {}}/>
+                        <span>{note.name}</span>
+                        <div className="delete"><button onClick={() => deleteNotes(index)}>✕</button></div>
+                    </div>
+                )
+            })}
+            </div>
+        </div>
+
+    );
+
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className="header">
+           СПИСОК ДЕЛ
+        </div>
+    );
+}
+
+export default Header;
